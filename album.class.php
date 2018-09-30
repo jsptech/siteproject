d4ce322080e6d1d3da40a873dab4ba346e93f611
prabinbasnet2015 <prabinbasnet30@yahoo.com>
2018-09-30 15:11:29 +0545

    miner changes

---
+++ album.class.php
@@ -0,0 +1,110 @@
+<?php
+    require_once('dbconfig.php');
+    class ALBUM
+    {
+        private $conn;
+        public function __construct()
+        {
+            $database = new Database();
+            $db = $database->dbConnection();
+            $this->conn = $db;
+        }
+        
+        public function GetAllAlbum($sql)
+        {
+            $stmt = $this->conn->prepare($sql);
+            return $stmt;
+        }
+
+        public function CheckAlbumIfExists($album_title)
+        {
+            try
+            {     
+                $stmt = $this->conn->prepare($album_title);
+                return $stmt;
+            }
+            catch(PDOException $e)
+            {
+                echo $e->getMessage();
+            } 
+        }
+        public function CheckAlbumIfExists1($title_name)
+        {
+            try
+            {     
+                $stmt = $this->conn->prepare("SELECT * FROM album WHERE album_name=:album_name");
+                $stmt->bindparam(":album_name", $album_name);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                return $result;
+            }
+            catch(PDOException $e)
+            {
+                echo $e->getMessage();
+            } 
+        }
+        public function GetAlbumById($id)
+        {   try
+            {     
+                $stmt = $this->conn->prepare("SELECT * FROM album WHERE id=:id");
+                $stmt->bindparam(":id", $id);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                return $result;
+            }
+            catch(PDOException $e)
+            {
+                echo $e->getMessage();
+            } 
+        }
+       
+        public function CreateAlbum($album_title)
+        {            
+            try
+            {         
+                $stmt = $this->conn->prepare("INSERT INTO album(album_name) 
+                                                        VALUES(:album_title)");
+                $stmt->bindparam(":album_title", $album_title);
+                $stmt->execute();
+                return $stmt;
+            }
+            catch(PDOException $e)
+            {
+                echo $e->getMessage();
+            }
+        }
+
+        public function UpdateAlbum($id, $album_title)
+        {
+            try
+            {
+                $stmt = $this->conn->prepare("UPDATE album SET album_name=:album_title WHERE id=:id");
+                $stmt->bindparam(":id", $id);               
+                $stmt->bindparam(":album_title", $album_title);
+                
+                $stmt->execute();
+                return $stmt;
+            }
+            catch(PDOException $e)
+            {
+                echo $e->getMessage();
+            }
+        }
+
+        public function DeleteAlbum($id)
+        {
+            try
+            {
+                $stmt = $this->conn->prepare("DELETE FROM album WHERE id=:id");                
+                $stmt->bindparam(":id", $id);
+                $stmt->execute();
+                return $stmt;
+            }
+            catch(PDOException $e)
+            {
+                echo $e->getMessage();
+            }
+        }
+
+    }
+?>
